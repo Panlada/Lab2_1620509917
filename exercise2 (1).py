05c702d754a477d260d974dc4b6d843b54eefa10
Panlada <98300337+Panlada@users.noreply.github.com>
2022-11-15 22:00:32 +0700

    Add files via upload

---
+++ exercise2 (1).py
@@ -0,0 +1,14 @@
+
+def _sum(arr,n):
+
+
+    return(sum(arr))
+
+array1 = [5,7,9,11,13]
+print("this is array1 ",array1)
+n = len(array1)
+
+ans = _sum(array1,n)
+
+print("thi is Length of array 1",n)
+print("this is summary of array1",ans)
